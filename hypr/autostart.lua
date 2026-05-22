9e5404f6f93e05e6d018598ca2b9dc96c83cce2d
Damet24 <damet.2401@gmail.com>
2026-05-21 19:01:19 -0500

    update image and update intervals

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -1,5 +1,5 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd("waybar -c ~/Development/dotfiles/waybar/config.jsonc -s ~/Development/dotfiles/waybar/style.css")
 	hl.exec_cmd("~/.local/bin/awww-daemon")
-	hl.exec_cmd("~/.local/bin/awww img ~/Pictures/Wallpapers/geisha_original.png")
+	hl.exec_cmd("~/.local/bin/awww img ~/Pictures/Wallpapers/samurai_original.png")
 end)
